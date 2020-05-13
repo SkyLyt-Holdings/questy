1df173eeac32994c24781158baa177a35049d7e3
Oleg Novoselskyy <oleg.novoselskyy@gmail.com>
2020-05-13 18:36:01 -0400

    QUEST-9 - Create Quest class

---
+++ Questy/Questy/Questy.Data/Migrations/20200513223430_quest-9.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Questy.Data.Migrations
+{
+    public partial class quest9 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Quests",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(nullable: true),
+                    StartDate = table.Column<DateTime>(nullable: true),
+                    EndDate = table.Column<DateTime>(nullable: true),
+                    AuditUser = table.Column<string>(nullable: true),
+                    LastUpdated = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quests", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Quests");
+        }
+    }
+}
