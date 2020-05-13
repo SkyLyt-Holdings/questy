229f319c4609a00ce34c633d74404569c8c55069
Oleg Novoselskyy <oleg.novoselskyy@gmail.com>
2020-05-13 19:02:27 -0400

    Quest-9 - Modified user class and dropped scripts

---
+++ Questy/Questy/Questy.Data/Migrations/20200513230124_quest-9.1.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Questy.Data.Migrations
+{
+    public partial class quest91 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_UserTypes_UserTypeID",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserTypeID",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_UserTypes_UserTypeID",
+                table: "Users",
+                column: "UserTypeID",
+                principalTable: "UserTypes",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_UserTypes_UserTypeID",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserTypeID",
+                table: "Users",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_UserTypes_UserTypeID",
+                table: "Users",
+                column: "UserTypeID",
+                principalTable: "UserTypes",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- Questy/Questy/Questy.Domain/Entities/User/User.cs
+++ Questy/Questy/Questy.Domain/Entities/User/User.cs
@@ -10,6 +10,7 @@ namespace Questy.Domain.Entities
         public string Username { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
+        public int UserTypeID { get; set; }
         public UserType UserType { get; set; }
         public bool isActive { get; set; }
         public string AuditUser { get; set; }
