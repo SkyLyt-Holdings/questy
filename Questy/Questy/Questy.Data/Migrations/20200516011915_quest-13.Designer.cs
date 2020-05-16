cf9ee1eb4ff2cf54d07bfe1d5ad2f4afd72a2066
Dmytro Lytvyn <dlytpi@gmail.com>
2020-05-15 21:22:54 -0400

    QUEST-13

--- Questy/Questy/Questy.Data/Migrations/20200516011915_quest-13.Designer.cs
+++ Questy/Questy/Questy.Data/Migrations/20200516011915_quest-13.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Questy.Data;
 
 namespace Questy.Data.Migrations
 {
     [DbContext(typeof(QuestyContext))]
-    partial class QuestyContextModelSnapshot : ModelSnapshot
+    [Migration("20200516011915_quest-13")]
+    partial class quest13
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
