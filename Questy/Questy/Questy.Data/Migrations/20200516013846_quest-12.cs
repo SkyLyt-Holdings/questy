19af5e187a086e0b4a76236b0dec2528bf65e0d6
Dmytro Lytvyn <dlytpi@gmail.com>
2020-05-15 21:39:53 -0400

    QUEST-12

---
+++ Questy/Questy/Questy.Data/Migrations/20200516013846_quest-12.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Questy.Data.Migrations
+{
+    public partial class quest12 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "QuestTag",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    QuestID = table.Column<int>(nullable: false),
+                    TagID = table.Column<int>(nullable: false),
+                    AuditUser = table.Column<string>(nullable: true),
+                    LastUpdated = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuestTag", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_QuestTag_Quests_QuestID",
+                        column: x => x.QuestID,
+                        principalTable: "Quests",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_QuestTag_Tags_TagID",
+                        column: x => x.TagID,
+                        principalTable: "Tags",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuestTag_QuestID",
+                table: "QuestTag",
+                column: "QuestID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuestTag_TagID",
+                table: "QuestTag",
+                column: "TagID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "QuestTag");
+        }
+    }
+}
---
+++ Questy/Questy/Questy.Domain/Entities/Quest/QuestTag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questy.Domain.Entities
+{
+    public class QuestTag
+    {
+        public int ID { get; set; }
+        public int QuestID { get; set; }
+        public int TagID { get; set; }
+        public Quest Archetype { get; set; }
+        public Tag Tag { get; set; }
+        public string AuditUser { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
