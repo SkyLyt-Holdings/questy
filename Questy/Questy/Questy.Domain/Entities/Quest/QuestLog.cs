c9e8f962cc96e9b25a862df3a5cd5a0629d36e9d
Simon Novoselskyy <semnovos@gmail.com>
2020-05-17 20:36:07 -0400

    added QuestLog class

---
+++ Questy/Questy/Questy.Domain/Entities/Quest/QuestLog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Questy.Domain.Entities
+{
+    public class QuestLog
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; } // FK to Users table
+        public int QuestID { get; set; } // FK to Quests table
+        public User User { get; set; }
+        public Quest Quest { get; set; }
+        [Required]
+        public bool IsCompleted { get; set; } = false;
+        public string AuditUser { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
