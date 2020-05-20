1672f79cefb53daf20e8c638884e75228b8d1686
Oleg Novoselskyy <oleg.novoselskyy@gmail.com>
2020-05-20 17:26:40 -0400

    QUEST-15

--- Questy/Questy/Questy.Domain/Entities/User/User.cs
+++ Questy/Questy/Questy.Domain/Entities/User/User.cs
@@ -6,12 +6,17 @@ namespace Questy.Domain.Entities
 {
     public class User
     {
+        public User()
+        {
+            QuestLog = new List<QuestLog>();
+        }
         public int ID { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
         public int UserTypeID { get; set; }
         public UserType UserType { get; set; }
+        public List<QuestLog> QuestLog { get; set; }
         public bool isActive { get; set; }
         public string AuditUser { get; set; }
         public DateTime LastUpdated { get; set; }
