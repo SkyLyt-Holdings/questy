2824126d60a3246ba94db99b6cfe1d2ae5e2b98a
Oleg Novoselskyy <oleg.novoselskyy@gmail.com>
2021-06-14 17:33:04 -0400

    QUEST-27 - Set up testing environment

---
+++ Questy/Questy/Questy.Tests/API/UsersShould.cs
@@ -0,0 +1,46 @@
+﻿using Questy.API;
+using Questy.API.Controllers.User.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Questy.Tests.API
+{
+    public class UsersShould : IClassFixture<TestFixture<Startup>>
+    {
+        private HttpClient Client;
+
+        public UsersShould(TestFixture<Startup> fixture)
+        {
+            Client = fixture.Client;
+            Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ContentHelper.GetToken());
+        }
+
+        [Fact]
+        public async Task LogInSuccessfully()
+        {
+            // Arrange
+            var request = new
+            {
+                Url = "/api/users/login",
+                Body = new UserRequestDTO
+                {
+                    Username = "Slimelord",
+                    Password = "Revan456lord!"
+                }
+            };
+
+            // Act
+            var response = await Client.PostAsync(request.Url, ContentHelper.GetStringContent(request.Body));
+            var content = response.Content.ReadAsStringAsync().Result;
+
+            // Assert
+            Assert.Contains("token", content);
+        }
+    }
+}
